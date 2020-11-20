6d99cbe13f7da8845d05f539414e4f04e5b56d08
Gabriel Chazanov <gabe.chaz@gmail.com>
2020-11-20 11:10:21 -0500

    commit tree efdf520764c8aa52ff5506948946432b5ecb2838

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
-
+    attr_accessor :balance, :status
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def deposit(money)
+        self.balance += money
+    end
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+    def valid?
+        if self.status == "open" && self.balance > 0
+            true
+        else
+            false
+        end
+    end
+    def close_account
+        self.status = "closed"
+    end
 end
